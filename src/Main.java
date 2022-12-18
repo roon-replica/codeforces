e2573698d1211d2b2ca4f62b64873ca93958bc23
leemr <think-roon@naver.com>
2022-12-18 22:33:29 +0900

    [Main 템플릿] generic하게 수정

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,10 @@ public class Main {
         Queue<Integer> q = new LinkedList<Integer>();
         PriorityQueue<pii<Integer, Integer>> pq = new PriorityQueue<>();
 
-        fs.next();
+    }
+
+    private static void howToUse(FastScanner fs, PriorityQueue<pii<Integer, Integer>> pq) {
+        // fs.next();
 
         var min = min(List.of(11, 22, 33));
         System.out.println(min);
@@ -26,67 +29,76 @@ public class Main {
         var ascList = Arrays.asList(11, 22, 33);
         rsort(ascList);
         System.out.println(ascList);
+        System.out.println(max(ascList));
 
         System.out.println(lowerBound(descList, 15));
 
         pq.add(new pii<>(1, 2));
+        pq.add(new pii<>(3, 4));
+
+        System.out.println(pq.poll());
+
+        System.out.println(gcd(2 * 2 * 3 * 5, 3 * 5));
+        System.out.println(gcd(2 * 3 * 5 * 7 * 11L, 2 * 5 * 11L));
+
+        System.out.println(abs(-11) + " " + abs(-1.123) + " " + abs(-12L));
     }
 
 
     /***********************************************************************/
-    static int gcd(int a, int b) {
-        if (b != 0) return gcd(b, a % b);
-        else return a;
+    static <T extends Number> T gcd(T a, T b) {
+        if (a instanceof Integer) {
+            if (b.intValue() != 0) return (T) gcd(b, a.intValue() % b.intValue());
+            else return a;
+        } else if (a instanceof Long) {
+            if (b.longValue() != 0) return (T) gcd(b, a.longValue() % b.longValue());
+            else return a;
+        } else {
+            throw new IllegalArgumentException("[gcd] Unsupported data type. only support int, long.");
+        }
     }
 
-    static long gcd(long a, long b) {
-        if (b != 0) return gcd(b, a % b);
-        else return a;
-    }
-
-    static void swap(int[] a, int idx1, int idx2) {
-        int tmp = a[idx1];
+    static <T> void swap(T[] a, int idx1, int idx2) {
+        T tmp = a[idx1];
         a[idx1] = a[idx2];
         a[idx2] = tmp;
     }
 
-    static void sort(int[] a) {
-        List<Integer> l = new ArrayList<>();
-        for (int i : a) l.add(i);
-        Collections.sort(l);
-        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    static <T extends Comparable<T>> void sort(T[] a) {
+        List<T> list = new ArrayList<>(Arrays.asList(a));
+        Collections.sort(list);
+        for (int i = 0; i < a.length; i++) a[i] = list.get(i);
     }
 
     static <T extends Comparable<T>> void sort(List<T> a) {
         Collections.sort(a);
     }
 
-    static void rsort(int[] a) {
-        List<Integer> l = new ArrayList<>();
-        for (int i : a) l.add(i);
-        Collections.sort(l, Collections.reverseOrder());
-        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    static <T extends Comparable<T>> void rsort(T[] a) {
+        List<T> list = new ArrayList<>(Arrays.asList(a));
+        list.sort(Collections.reverseOrder());
+        for (int i = 0; i < a.length; i++) a[i] = list.get(i);
     }
 
     static <T extends Comparable<T>> void rsort(List<T> a) {
         a.sort(Collections.reverseOrder());
     }
 
-    static int lowerBound(int[] a, int x) {
+    static <T extends Comparable<T>> int lowerBound(T[] a, T x) {
         int s = 0, e = a.length;
         while (s < e) {
             int mid = (s + e) / 2;
-            if (a[mid] >= x) e = mid;
+            if (a[mid].compareTo(x) >= 0) e = mid;
             else s = mid + 1;
         }
         return e;
     }
 
-    static int lowerBound(List<Integer> a, int x) {
+    static <T extends Comparable<T>> int lowerBound(List<T> a, T x) {
         int s = 0, e = a.size();
         while (s < e) {
             int mid = (s + e) / 2;
-            if (a.get(mid) >= x) e = mid;
+            if (a.get(mid).compareTo(x) >= 0) e = mid;
             else s = mid + 1;
         }
         return e;
@@ -133,7 +145,7 @@ public class Main {
         }
     }
 
-    static class pii<F, S> implements Comparable<pii<F, S>> {
+    static class pii<F extends Comparable<F>, S extends Comparable<S>> implements Comparable<pii<F, S>> {
         final F fs;
         final S sc;
 
@@ -142,65 +154,64 @@ public class Main {
             this.sc = sc;
         }
 
-        static <F, S> pii<F, S> of(F fs, S sc) {
-            return new pii<F, S>(fs, sc);
+        static <F extends Comparable<F>, S extends Comparable<S>> pii of(F fs, S sc) {
+            return new pii<>(fs, sc);
         }
 
         public int compareTo(pii<F, S> o) {
-            if ((int) this.fs < (int) o.fs) return -1;
-            else if ((int) this.fs == (int) o.fs) {
-                if ((int) this.sc <= (int) o.sc) return -1;
-                else return 1;
-            }
-            return 1;
+            return this.fs.compareTo(o.fs);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("{fs: %s, sc: %s}\n", this.fs, this.sc);
         }
     }
 
-    static int abs(int a) {
-        return Math.abs(a);
+    public static <T extends Number> T abs(T x) {
+        if (x instanceof Integer) {
+            return (T) (Object) Math.abs((Integer) x);
+        }
+        if (x instanceof Long) {
+            return (T) (Object) Math.abs((Long) x);
+        }
+        if (x instanceof Double) {
+            return (T) (Object) Math.abs((Double) x);
+        }
+        throw new IllegalArgumentException("[abs] Unsupported data type. only support int, long, double");
     }
 
-    static long abs(long a) {
-        return Math.abs(a);
+    static <T extends Comparable<T>> T min(T a, T b) {
+        if (a.compareTo(b) < 0) return a;
+        else return b;
     }
 
-    static int min(int a, int b) {
-        return Math.min(a, b);
+    static <T extends Comparable<T>> T min(T[] a) {
+        T min = a[0];
+        for (int i = 1; i < a.length; i++) if (min.compareTo(a[i]) > 0) min = a[i];
+        return min;
     }
 
-    static long min(long a, long b) {
-        return Math.min(a, b);
+    static <T extends Comparable<T>> T min(List<T> a) {
+        T min = a.get(0);
+        for (int i = 1; i < a.size(); i++) if (min.compareTo(a.get(i)) > 0) min = a.get(i);
+        return min;
     }
 
-    static int min(int[] a) {
-        int mn = a[0];
-        for (int i = 1; i < a.length; i++) if (mn > a[i]) mn = a[i];
-        return mn;
+    static <T extends Comparable<T>> T max(T a, T b) {
+        if (a.compareTo(b) >= 0) return a;
+        else return b;
     }
 
-    static int min(List<Integer> a) {
-        int mn = a.get(0);
-        for (int i = 1; i < a.size(); i++) if (mn < a.get(i)) mn = a.get(i);
-        return mn;
+    static <T extends Comparable<T>> T max(T[] a) {
+        T max = a[0];
+        for (int i = 1; i < a.length; i++) if (max.compareTo(a[i]) < 0) max = a[i];
+        return max;
     }
 
-    static int max(int a, int b) {
-        return Math.max(a, b);
-    }
-
-    static long max(long a, long b) {
-        return Math.max(a, b);
-    }
-
-    static int max(int[] a) {
-        int mx = a[0];
-        for (int i = 1; i < a.length; i++) if (mx < a[i]) mx = a[i];
-        return mx;
-    }
-
-    static int max(List<Integer> a) {
-        int mx = a.get(0);
-        for (int i = 1; i < a.size(); i++) if (mx > a.get(i)) mx = a.get(i);
-        return mx;
+    static <T extends Comparable<T>> T max(List<T> a) {
+        T max = a.get(0);
+        for (int i = 1; i < a.size(); i++) if (max.compareTo(a.get(i)) < 0) max = a.get(i);
+        return max;
     }
 }
