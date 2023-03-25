1fd6a8f46b2087b7c6d6c77edf544ef599c9c578
roon <think-roon@naver.com>
2023-03-26 00:32:33 +0900

    [Boilerplates] Main 클래스

--- src/bolierplates/Main.java
+++ src/bolierplates/Main.java
@@ -1,3 +1,5 @@
+package bolierplates;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
---
+++ src/bolierplates/VerySimpleMain.java
@@ -0,0 +1,21 @@
+package bolierplates;
+
+import java.util.Scanner;
+
+public class VerySimpleMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tt = scanner.nextInt();
+
+        for (int tc = 0; tc < tt; tc++) {
+            int n = scanner.nextInt();
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+
+        }
+
+    }
+}
