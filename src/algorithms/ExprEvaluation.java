b3aa87cf197680b6a22109294efab8c9deb84b6e
roon-replica <think-roon@naver.com>
2023-10-09 16:59:41 +0900

    [알고리즘] 수식의 평가 - 괄호있을 때 infix -> postfix 변환
    - 이건 공식을 외우고 있어야 구현 가능..
      postfix 변환을 구현하라고 하는것보단 변환 공식을 주고 공식대로 구현하라고 하는게 합리적인 문제일듯
    - 군더더기 많은 구현인듯
    
    - 문제 분류, 난이도:
    - 웹 페이지
      - https://www.geeksforgeeks.org/convert-infix-expression-to-postfix-expression/
      - https://github.com/TheAlgorithms/Java/blob/master/src/main/java/com/thealgorithms/stacks/InfixToPostfix.java

--- src/algorithms/ExprEvaluation.java
+++ src/algorithms/ExprEvaluation.java
@@ -1,8 +1,12 @@
 package algorithms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
 
 public class ExprEvaluation {
 
@@ -10,8 +14,82 @@ public class ExprEvaluation {
 		var scanner = new Scanner(System.in);
 		var expr = scanner.next();
 
-		evalNoParenthesis(expr);
+		System.out.println(infix2Postfix(expr));
+		;
+		// evalNoParenthesis(expr);
 	}
+
+
+	private static void evalWithParenthesis(String expr) {
+
+	}
+
+	private static String infix2Postfix(String expr) {
+		/* 알고리즘 (associativity는 고려하지 않음. 즉, +,-,*,/, (, )만 고려)
+		1. 스캔
+		  1-1. 숫자일때 -> postfix 수식에 넣기
+		  1-2. 연산자일 때
+		    a) 현재 연산자가 stack 최상단의 연산자보다 우선순위가 높다, empty, ( 인 경우 -> push
+		    b) 현재 연산자가 더 우선순위가 낮거나 같다 -> 그런 연산자 모두 pop하고 나서 현재 연산자 push
+		  1-3. 여는 괄호일 때 -> push
+		  1-4. 닫는 괄호일 때 -> (가 나올때까지 pop. 괄호들은 버림
+		2. pop all
+
+		ex. 123+(5-3)*4
+		*/
+
+		var opStack = new Stack<String>();
+		var operators = new HashSet<>(Set.of('+', '-', '*', '/'));
+		var priorityMap = new HashMap<String, Integer>();
+		priorityMap.put("+", 1);
+		priorityMap.put("-", 1);
+		priorityMap.put("*", 2);
+		priorityMap.put("/", 2);
+
+		StringBuilder postfix = new StringBuilder();
+		String value = "";
+
+		for (int i = 0; i < expr.length(); i++) {
+			var ch = expr.charAt(i);
+
+			if (Character.isDigit(ch)) {
+				value += ch;
+				if (i == expr.length() - 1) {
+					postfix.append(" "+value + " ");
+				}
+			} else {
+				if (!value.equals("")) {
+					postfix.append(value + " ");
+					value = "";
+				}
+
+				if (operators.contains(ch)) {
+					while (!(opStack.isEmpty() || opStack.peek().equals("(") || priorityMap.get("" + ch) > priorityMap.get(opStack.peek()))) {
+						postfix.append(opStack.pop());
+					}
+
+					opStack.push("" + ch);
+				} else if (ch == '(') {
+					opStack.push("" + ch);
+				} else if (ch == ')') {
+					while (!opStack.isEmpty()) {
+						var popped = opStack.pop();
+						if (popped.equals("(")) {
+							break;
+						}
+						postfix.append(popped + " ");
+					}
+				}
+			}
+		}
+
+		while (!opStack.isEmpty()) {
+			postfix.append(opStack.pop()).append(" ");
+		}
+
+		return postfix.toString();
+	}
+
 
 	private static void evalNoParenthesis(String expr) {
 		// +, -, *, / 포함 가능하고 괄호는 없는 수식
