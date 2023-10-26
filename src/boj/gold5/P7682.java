026efce39c150f272caeb109ed04b39ea4bd7a2f
roon-replica <think-roon@naver.com>
2023-10-26 20:24:06 +0900

    [BOJ-7682] 틱택토
    - 완벽한 구현이 만만치않다
      아.. 접근법이 잘못됨?
      케이스를 분류하고 상태가 유효한지 체크하는 방식으로는 다 체크하기 까다롭다
    
    - 가능한 답을 다 구해놓고 거기에 포함되는지 판단해보는 방식으로?
      시간이나 공간이 안된다?
    
    - 아,.... 첫번째 접근이 얼추 맞았네
      근데 구현도 그리 복잡하지 않게 간단히 체크할 방법이 있었다.......................
      적절한 분류 기준을 잘 생각해냈어야 했는데 너무 복잡한 기준으로 접근했다
      복잡한 이유가 딱 맞는 톱니가 아닌데 억지로 끼워맞추려다보니 복잡해진듯
    
    - 문제 분류, 난이도: 구현, 골드 5
    - 웹 페이지: https://www.acmicpc.net/problem/7682

---
+++ src/boj/gold5/P7682.java
@@ -0,0 +1,120 @@
+package boj.gold5;
+
+import java.util.Scanner;
+
+public class P7682 {
+
+	public static void main(String[] args) {
+		// .xx
+		// x.x
+		// ooo
+
+		// oxo
+		// xox
+		// oxo
+
+		// x.o
+		// o..
+		// x..
+
+		// 1. o 승 -> n(o) == n(x) 여야함
+		// 2. x 승 -> n(x) = n(o)+1
+		// 3. 승부가 안났다
+		//  .가 있다 -> invalid
+		//  .가 없다 -> n(x) = n(o)+1
+		// 4. o승 & x승 -> invalid
+		// 3개 연속 판단 함수
+
+		var scanner = new Scanner(System.in);
+
+		for (var s = scanner.next(); !s.equals("end"); s = scanner.next()) {
+			char[][] map = new char[3][3];
+			int cntO = 0;
+			int cntX = 0;
+
+			for (int r = 0; r < 3; r++) {
+				for (int c = 0; c < 3; c++) {
+					map[r][c] = s.charAt(r * 3 + c);
+
+					if (map[r][c] == 'O') {
+						cntO++;
+					} else if (map[r][c] == 'X') {
+						cntX++;
+					}
+				}
+			}
+
+			boolean dot = (cntO + cntX != 3 * 3);
+
+			int winO = countWin(map, 'O');
+			int winX = countWin(map, 'X');
+
+			boolean answer = true;
+			if (winO > 0 && winX > 0) {
+				answer = false;
+			} else if (winO > 0) {
+				answer = (cntO == cntX);
+			} else if (winX > 0) {
+				answer = (cntX == cntO + 1);
+			} else { // winO =0 && winX = 0
+				if (dot) {
+					answer = false;
+				} else {
+					answer = (cntX == cntO + 1);
+				}
+			}
+
+			System.out.println(answer ? "valid" : "invalid");
+
+
+		}
+	}
+
+	static int[] dr = {-1, -1, -1, 0, 1, 1, 1, 0}; //좌상부터
+	static int[] dc = {-1, 0, 1, 1, 1, 0, -1, -1};
+
+	static int countWin(char[][] map, char query) {
+		int ret = 0;
+
+		for (int r = 0; r < 3; r++) {
+			for (int c = 0; c < 3; c++) {
+				char here = map[r][c];
+				if (here != query) {
+					continue;
+				}
+
+				int sameChCnt;
+
+				for (int dir = 0; dir < dr.length; dir++) {
+					sameChCnt = 0;
+
+					for (int d = 0; d <= 2; d++) {
+						int nr = r + dr[dir] * d;
+						int nc = c + dc[dir] * d;
+
+						if (nr < 0 || nr > 2 || nc < 0 || nc > 2) {
+							break;
+						}
+
+						if (here != map[nr][nc]) {
+							break;
+						} else {
+							sameChCnt++;
+						}
+					}
+
+					if (sameChCnt == 3) {
+						ret++;
+					}
+				}
+			}
+		}
+
+		if (ret % 2 == 1) {
+			throw new RuntimeException("never");
+		}
+		return ret / 2;
+	}
+
+
+}
