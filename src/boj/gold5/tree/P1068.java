feac8774ee5723b3a53d114ef2a8d9cd5bc9c4fa
roon-replica <think-roon@naver.com>
2023-11-17 19:40:01 +0900

    [BOJ-1068] 트리
    - naive하게 구현하면 돼서 쉬운듯?
      각 노드에 child count 계산해두면 답 = 총합 - 지울노드 count
      leaf들 찾고 leaf -> root로 타고 올라가는 누적합 방식으로 child count 계산하면 되겠지
    - 왜 틀림?
      아.. 어떤 노드를 지워도.. 그 줄기는 남아있는거라면 그 부모만큼의 count에서 빼기?
      단순히 count로는 못하겠다. 틀리는 케이스가 있나봄
      시뮬레이션하듯이 해야할듯. n도 작은데 안전하게 시뮬하는게 나았겠다.. 이렇게 틀릴거면
    
     - 재시도
       다시 간단하게 생각하면 트리 구성하기?, 어떤 노드와 아래의 노드들을 제거하는 동작, 어떤 트리의 리프 노드들을 세아리는 동작
       이 3 가지만 구현하면 걍 풀리겠지
       입력 범위도 진짜 작다
    
       와.... 진짜 단순하게 푸는게 짱인듯
       엄청 쉬운 문젠데 엉성하게 최적화해서 풀겠다고 제대로 동작하지도 않게 짰네
    
    - 문제 분류, 난이도: 트리, 골드5
    - 웹 페이지: https://www.acmicpc.net/problem/1068

---
+++ src/boj/gold5/tree/P1068.java
@@ -0,0 +1,76 @@
+package boj.gold5.tree;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class P1068 {
+
+	public static void main(String[] args) {
+		var scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+		var parent = new int[n];
+		for (int i = 0; i < n; i++) {
+			parent[i] = scanner.nextInt();
+		}
+		int remove = scanner.nextInt();
+
+		var q = new LinkedList<Integer>();
+		q.add(remove);
+		var removed = new boolean[n];
+
+		for (; !q.isEmpty(); ) {
+			int here = q.poll();
+			removed[here] = true;
+
+			for (int i = 0; i < n; i++) {
+				if (parent[i] == here) {
+					q.add(i);
+				}
+			}
+		}
+
+//		for (int i = 0; i < n; i++) {
+//			System.out.print(removed[i] + " ");
+//		}
+//		System.out.println();
+
+		int answer = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (removed[i]) {
+				continue;
+			}
+
+			boolean isLeaf = true;
+			for (int j = 0; j < n; j++) {
+				if(removed[j]) continue;
+
+				if (parent[j] == i) {
+					isLeaf = false;
+					break;
+				}
+			}
+
+			if(isLeaf){
+				answer++;
+			}
+		}
+
+		System.out.println(answer);
+
+
+	}
+
+	static class Node {
+
+		int node;
+		int count;
+
+		public Node(int node, int count) {
+			this.node = node;
+			this.count = count;
+		}
+	}
+
+}
