c888835b8bf6de3f27774f545de052b0a374073e
roon-replica <think-roon@naver.com>
2023-11-18 22:46:09 +0900

    [BOJ-15681] 트리와 쿼리
    - dfs로 하니까 정상 동작은 하는데 메모리 많이 써서 겨우 통과한다
    - visited 배열, tree 배열 별도로 쓸 필요없이 인접리스트를 바로 이용할 수 있네
    - int dfs()도 되는데 void dfs()도 가능하더라
    
    - 문제 분류, 난이도: 트리, 골드 5
    - 웹 페이지: https://www.acmicpc.net/problem/15681

--- src/boj/gold5/tree/P15681.java
+++ src/boj/gold5/tree/P15681.java
@@ -1,12 +1,15 @@
 package boj.gold5.tree;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class P15681 {
+
+	static int MAX = 100001;
+	static List<Integer>[] adjList = new ArrayList[MAX];
+	static int[] count = new int[MAX];
 
 	public static void main(String[] args) {
 		var scanner = new Scanner(System.in);
@@ -15,7 +18,6 @@ public class P15681 {
 		int ROOT = scanner.nextInt();
 		int Q = scanner.nextInt();
 
-		List<Integer>[] adjList = new ArrayList[n + 1];
 		for (int i = 1; i <= n; i++) {
 			adjList[i] = new ArrayList();
 		}
@@ -27,75 +29,7 @@ public class P15681 {
 			adjList[v].add(u);
 		}
 
-		var visited = new boolean[n + 1];
-		var q = new LinkedList<Integer>();
-		q.add(ROOT);
-
-		var parent = new int[n + 1];
-
-		// 1. 트리 구성
-		for (; !q.isEmpty(); ) {
-			var here = q.poll();
-
-			for (int child : adjList[here]) {
-				if (visited[child]) {
-					continue;
-				}
-
-				parent[child] = here;
-				q.add(child);
-			}
-
-			visited[here] = true;
-		}
-
-//		for (int i = 1; i <= n; i++) {
-//			System.out.print(parent[i] + " ");
-//		}
-//		System.out.println();
-
-		var hasChild = new boolean[n + 1];
-		for (int i = 1; i <= n; i++) {
-			hasChild[parent[i]] = true;
-		}
-
-		var q2 = new PriorityQueue<Node>();
-		var count = new int[n + 1];
-
-		for (int i = 1; i <= n; i++) {
-			count[i] = 1;
-			if (!hasChild[i]) {
-				q2.add(new Node(n + 1, i));
-			}
-		}
-
-		for (int i = 1; i <= n; i++) {
-			visited[i] = false;
-		}
-
-		// 2. 미리 계산. 트리를 타고 올라가며
-		while (!q2.isEmpty()) {
-			var here = q2.poll();
-
-			int par = parent[here.node];
-			count[par] += count[here.node];
-
-			if (par == ROOT) {
-				continue;
-			}
-
-			if (!visited[par]) {
-				q2.add(new Node(here.depth - 1, par));
-				visited[par] = true;
-			}
-
-			visited[here.node] = true;
-		}
-
-//		for(int i=1;i<=n;i++){
-//			System.out.print(count[i]+" ");
-//		}
-//		System.out.println();
+		dfs(ROOT, -1);
 
 		for (int i = 0; i < Q; i++) {
 			int root = scanner.nextInt();
@@ -103,20 +37,21 @@ public class P15681 {
 		}
 	}
 
-	private static class Node implements Comparable<Node> {
+	static int dfs(int here, int parent) {
+		count[here] = 1;
 
-		int depth;
-		int node;
+		for (int child : adjList[here]) {
+			if (child == parent) {
+				continue;
+			}
 
-		Node(int depth, int node) {
-			this.depth = depth;
-			this.node = node;
+			if (count[child] != 0) {
+				count[here] += count[child];
+			} else {
+				count[here] += dfs(child, here);
+			}
 		}
 
-		@Override
-		public int compareTo(Node o) {
-			return -Integer.compare(this.depth, o.depth);
-		}
+		return count[here];
 	}
-
 }
