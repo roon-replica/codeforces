6e1eaec6bfc52c2fc61b970428c00b84d0d72f44
roon-replica <think-roon@naver.com>
2025-02-09 18:08:11 +0900

    동전1 - 성공한 풀이
    - 일단 하나하나 따지며 생각하는걸 귀찮아 하면 안되는거 같고..
      한번에 답이 나오길 기대하면 안되는거 같고...
    - 근데 이거 관찰? 규칙이 안보이면 못풀거 같기도 하고..
    
    - 문제 분류: dp
    - 난이도(실제/체감): 어려움
    - 웹 페이지: https://www.acmicpc.net/problem/2293

--- src/boj/after2502/p2293/Main.java
+++ src/boj/after2502/p2293/Main.java
@@ -1,60 +1,51 @@
 package boj.after2502.p2293;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//n*k. 동전 개수 늘려가면서 값 계산하고 ans++
-		// dfs
-
 		var scanner = new java.util.Scanner(System.in);
 		n = scanner.nextInt();
 		k = scanner.nextInt();
-		arr = new int[n];
-		var list = new ArrayList<Integer>();
-		list.add(0);
+		coins = new int[n];
 
 		for (int i = 0; i < n; i++) {
-//			arr[i] = scanner.nextInt();
-			list.add(scanner.nextInt());
+			coins[i] = scanner.nextInt();
 		}
+		Arrays.sort(coins);
 
+		// dp[값] = 경우의 수로 하면
+		// dp[k] = dp[k-coin1] + dp[k-coin2] + dp[k-coin3] + ... + dp[k-coinN] 일텐데
+		// 겹치는 경우를 어떻게 하지? (2,1)과 (1,2)을 구분하면 안되는데
+		// dp[0] = 0
+		// coin이 정렬되어 있으면 dp[coin1] = 1로 둬도 되겠고
+		// dp[coin2] = 1 + dp[coin2-coin1]
+		// dp[coin3] = 1 + dp[coin3-coin1] + dp[coin3-coin2]
 
-		int[][] dp = new int[n + 1][k + 1];
-		dp[0][0]=1;
+		// 못풀겠어서 풀이 찾아봄
+		// https://velog.io/@jxlhe46/%EB%B0%B1%EC%A4%80-2293%EB%B2%88.-%EB%8F%99%EC%A0%84-1-bfi120m5
+		// 여기보니까 {1,2}만 쓰는 경우 직접 나열해보면 규칙보이는거 같기도...
 
-		for (int i = 1; i <= n; i++) {
-//			for(int val=0;val<=k;val++) {
-//				dp[i+1][val]+=dp[i][val];
-//			}
-			
-			for(int bi=0;bi<i;bi++){
-				for (int a : list) {
-					for (int val = 0; val <= k; val++) {
-						if (val - a >= 0) {
-							dp[i][val] += dp[bi][val - a];
-						}
-					}
+		int[] dp = new int[k + 1];
+		dp[0] = 1;
+
+		for (int coin : coins) {
+			for (int val = 0; val < k; val++) {
+				if (val + coin > k) {
+					break;
 				}
+				dp[val + coin] += dp[val];
 			}
-
-
 		}
 
-		System.out.println(dp[n][k]);
+//		for (int i = 1; i <= k; i++) {
+//			System.out.print(dp[i] + " ");
+//		}
 
-		for (int i = 0; i <= n; i++) {
-			for (int v = 0; v <= k; v++) {
-				System.out.print(dp[i][v] + " ");
-			}
-			System.out.println();
-		}
-
-//		System.out.println(ans);
-
+		System.out.println(dp[k]);
 	}
 
 	static int n, k, value, ans;
-	static int[] arr;
+	static int[] coins;
 }
